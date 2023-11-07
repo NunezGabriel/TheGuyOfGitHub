f18f9bfe0c620dfa8b2c20d5f564a89c735e62f5
NunezGabriel <Tugabriel.nunez.arenas@gmail.com>
2023-11-07 11:22:37 -0500

    update project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
    <>
-    <h1>Hola mundo</h1>
+   <div className="w-full h-52 mx-auto flex items-center justify-center">
+    <h1 className="md:text-[90px] r">The Guy Of Github</h1>
+   </div>
    </>
   )
 }
