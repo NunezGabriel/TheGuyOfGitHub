63147e287be618794a67090469b3e6ecdfde687b
NunezGabriel <Tugabriel.nunez.arenas@gmail.com>
2023-11-08 08:44:48 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image"
-
 export default function Home() {
   return (
    <>
-   <div className="w-full h-52 mx-auto flex items-center justify-center">
-    <h1 className="md:text-[90px] r">The Guy Of Github</h1>
+   <div className="w-full mx-auto flex flex-col items-center justify-center p-4 text-center gap-10">
+    <h1 className="md:text-[90px] text-[50px]">The Guy Of Github</h1>
+    <p className="text-gray-400">Here you can see the stats of your account or diferent accounts and see if you o thos peaplo are THE GUY OF GITHUB✨</p>
    </div>
    </>
   )
