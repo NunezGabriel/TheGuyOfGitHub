a48b98a3030046bcc4cde2cbcaaed2b4faa65cc7
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2024-01-15 16:48:25 -0500

    adding fetch fundaments

---
+++ src/components/mainCardComp.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function MainCardComponent({ user }) {
+  //   const useUser = () => {
+  //     const [user, setUser] = useState(null);
+
+  //     const fetchUser = async () => {
+  //       const response = await fetch("https://api.github.com/users/NunezGabriel");
+
+  //       if (response.status === 200) {
+  //         const data = await response.json();
+  //         setUser(data);
+  //       }
+  //     };
+
+  //     useEffect(() => {
+  //       fetchUser();
+  //     }, []);
+
+  //     return user;
+  //   };
+
+  return (
+    <div
+      className=" p-6 justify-center
+        items-center text-md text-pretty font-light flex flex-col gap-10" // POSSIBLE BREAKPOINT IN FLEX DIRECTION
+    >
+      <div className="w-full p-10 gap-6 bg-gray-950 flex flex-col items-center rounded-lg text-white">
+        <div className="bg-red-600 w-36 h-36 rounded-full"></div>
+        <h1>Repositories: </h1>
+        <h1>Stars: </h1>
+        <h1>Followers: </h1>
+        <h1>Contributions: </h1>
+        <h1>Commits: </h1>
+        <h1>RESULT: </h1>
+      </div>
+    </div>
+  );
+}
+
+export default MainCardComponent;
--- src/pages/cardPage.jsx
+++ src/pages/cardPage.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import MainCardComponent from "@/components/mainCardComp";
 
 const CardPage = () => {
   const [value, setValue] = useState("");
@@ -24,16 +25,9 @@ const CardPage = () => {
         </div>
         <button className="py-1 px-2 rounded-xl w-20 bg-[#4dffcd]">Done</button>
       </div>
-      <div
-        className=" p-6 justify-center
-        items-center text-md text-pretty font-light flex flex-col gap-10" // POSSIBLE BREAKPOINT IN FLEX DIRECTION
-      >
-        <div className="w-full p-10 gap-6 bg-gray-950 flex flex-col items-center rounded-lg text-gray-950">
-          asdas
-        </div>
-      </div>
+      <MainCardComponent user={value} />
     </div>
   );
 };
-
+//mover eso a un componente donde se hara el fetch de ddatos pasando como props el value asi se hara bien
 export default CardPage;
