d5eb81f658f6f7e509282a3226864ccbc9b0cff6
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2024-01-29 16:25:38 -0500

    add: many new things

--- src/pages/cardPage.jsx
+++ src/pages/cardPage.jsx
@@ -26,7 +26,7 @@ const CardPage = () => {
 
   return (
     <div>
-      <div className=" p-6 justify-center items-center text-md text-pretty flex gap-4">
+      <div className=" p-6 justify-center items-center text-md text-pretty flex max-w-[500px] mx-auto gap-4">
         <div className="flex px-3 rounded-xl bg-gray-800 w-full">
           <input
             type="text"
@@ -53,37 +53,63 @@ const CardPage = () => {
 
       <div
         className=" p-6 justify-center
-        items-center text-md text-pretty font-light flex flex-col gap-10" // POSSIBLE BREAKPOINT IN FLEX DIRECTION
+        items-center text-md text-pretty font-light flex flex-col gap-10 " // POSSIBLE BREAKPOINT IN FLEX DIRECTION
       >
-        <div className="w-full p-10 gap-6 bg-gray-950 flex flex-col items-center rounded-lg text-white">
+        <div className="w-full p-10 gap-6 bg-gray-950 max-w-[1000px] flex flex-col md:flex-row md:justify-around md:gap-0  items-center rounded-lg text-white">
           {data ? (
             <Image
-              src={`${data.avatar_url}`}
-              className="w-36 h-36 rounded-full"
+              src={`${
+                data.avatar_url
+                  ? data.avatar_url
+                  : "/img/red-removebg-preview.png"
+              }`}
+              className="w-36 h-36 md:w-[350px] md:h-[350px] rounded-full"
               width={36}
               height={36}
               alt="user image"
             />
           ) : (
-            console.log("XD")
+            console.log("it doesn't work")
           )}
           {data ? (
-            <div className="flex flex-col gap-4 text-center">
-              <h1>User Name: {data.login}</h1>
-              <h1>Name: {data.name}</h1>
-              <h1>Public Repositories: {data.public_repos}</h1>
-              <h1>Stars: {data.login}</h1>
-              <h1>Followers: {data.followers}</h1>
+            <div className="flex flex-col gap-4 text-center md:text-start">
               <h1>
-                Blog:{" "}
+                <span className="text-[#9839ff]">User Name: </span>
+                {data.login}
+              </h1>
+              <h1>
+                <span className="text-[#9839ff]">Name: </span> {data.name}
+              </h1>
+              <h1>
+                <span className="text-[#9839ff]">Public Repositories: </span>
+                {data.public_repos}
+              </h1>
+              <h1>
+                <span className="text-[#9839ff]">Stars: </span>
+                {data.login}
+              </h1>
+              <h1>
+                <span className="text-[#9839ff]">Followers: </span>
+                {data.followers}
+              </h1>
+              <h1>
+                <span className="text-[#9839ff]">Blog: </span>
                 {data.blog ? (
-                  <Link href={`${data.blog}`}>✓ click to see it</Link>
+                  <Link href={`${data.blog}`} className="text-green-400">
+                    ✓ click to see it
+                  </Link>
                 ) : (
-                  <h1>No Blog</h1>
+                  <h1 className="text-red-500">✗ No Blog</h1>
                 )}
               </h1>
-              <h1>Commits: {data.login}</h1>
-              <h1>RESULT: {data.login}</h1>
+              <h1>
+                <span className="text-[#9839ff]">Commits: </span>
+                {data.login}
+              </h1>
+              <h1 className="text-2xl text-[#ff00d4]">
+                <span className="text-2xl">RESULT: </span>
+                {data.login}
+              </h1>
             </div>
           ) : (
             console.log("no funciono")
